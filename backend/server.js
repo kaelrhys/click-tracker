126b630387c989d2d54ba5ce39a3e004ede26304
Kael Matthews <kaelmatthews@gmail.com>
2019-06-29 12:18:27 +0800

    Add CORS and update save method on backend

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,35 @@ const logger = require('koa-morgan')
 const Router = require('koa-router')
 const bodyParser = require('koa-body')()
 const models = require('./models')
+const cors = require('@koa/cors');
 
 const server = new Koa();
 const router = new Router()
 
-router.post('/new/', bodyParser, async ctx => {
-    const Session = await models.Session.create({})
-    ctx.body = JSON.stringify(Session);
-});
+var corsOptions = {
+	origin: '*'
+};
 
-router.post('/save', bodyParser, async ctx => {
-        const session = await models.Session.findOne({where: { id: ctx.request.body.id }})
-        session.clicks = ctx.request.body.clicks
-        session.save()
-        ctx.body = JSON.stringify(session);
+router.post('/new/', bodyParser, async ctx => {
+    // const Session = await models.Session.create({})
+    ctx.body = JSON.stringify(ctx.request.body)
+})
+
+router.post('/save/', bodyParser, async ctx => {
+        let session;
+        if (ctx.request.body.sessionId) {
+            session = await models.Session.findOne({where: { id: ctx.request.body.sessionId }})
+            session.clicks = ctx.request.body.clicks
+            session.save()
+        } else {
+            session = await models.Session.create({ clicks: ctx.request.body.clicks })
+        }
+        ctx.body = JSON.stringify(session)
     }
-);
+)
 
 server
+.use(cors(corsOptions))
 .use(logger('tiny'))
 .use(router.routes())
 .listen(3001)
